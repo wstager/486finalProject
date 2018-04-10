f8d45436d499c1ccd78f2585aad9b7f9aeceee8f
wstager <willstager96@gmail.com>
2018-04-09 21:29:59 -0400

    crawler working

--- crawler.py
+++ crawler.py
@@ -7,52 +7,30 @@ import queue
 from lxml import html
 import urllib.parse
 import requests
-from newspaper import Article
+import newspaper
 
 
-def crawl(url, num_links, output):
+
+def parseText(text):
+    print(text)
+
+def crawl(url, num_links, output, site_name, score, politics_flag):
     visited = set()
     q = queue.Queue()
     q.put(url)
     count = 0
     visited.add(url)
-    #grabs domain from given url
-    allowed_domain = '{uri.scheme}://{uri.netloc}/'.format(uri=urllib.parse.urlparse(url))
-    with open('crawler.output.edges', 'w') as edge_output:
-        while (not q.empty()) and (count < num_links):
-            url = q.get()
-            try:
-                page = requests.get(url, timeout=3.0)
-            except:
-                #catches timeouts, any other possible errors
-                continue
-            if "html" in page.headers["Content-Type"]:
-                count+=1
-                output.write(url + '\n')
-                tree = html.fromstring(page.text).iterlinks()
-                for link in tree:
-                    #ignore non-html files
-                    if not any (ext in link[2] for ext in [".css", ".jpg", ".jpeg", ".docx", ".pdf", ".gif" ".ico", ".svg", ".ppt", ".js", ".png", ".cgi"]):
-                        link = link[2]
-                        #append relative URLs to parent URL
-                        if link.startswith("/"):
-                            link = urllib.parse.urljoin(allowed_domain,link)
-                        #pulls domain from link to make sure it matches target domain
-                        domain = '{uri.scheme}://{uri.netloc}/'.format(uri=urllib.parse.urlparse(link))
-                        #remove trailing / for consistancy
-                        link = link.rstrip('/')
-                        #make sure domain matches original URL
-                        if allowed_domain == domain:
-                            if link == url:
-                                continue
-                            #add to queue if not visited
-                            if link not in visited:
-                                visited.add(link)
-                                q.put(link)
-                                edge_output.write(url + "\t" + link+"\n")
-                            #store edges for pagerank
-                            elif link != url:
-                                edge_output.write(url + "\t" + link+"\n")
+    paper = newspaper.build(url, memoize_articles=False)
+    print(url)
+    for article in paper.articles:
+        if politics_flag and "politics" not in article.url:
+            continue
+        if count>num_links:
+            break
+            count+=1
+            article.download()
+            article.parse()
+            parseText(article.text)
 
 if __name__ == '__main__':
     assert(len(sys.argv) >= 2)
@@ -62,4 +40,10 @@ if __name__ == '__main__':
         with open(file_path, 'r') as curr_file:
             urls = [line.rstrip('\n') for line in curr_file]
             for url in urls:
-                crawl(url, num_links, output)
+                url = url.split('\t')
+                site_name = url[1]
+                score = url[2]
+                politics_flag = False
+                if url[3] == "yes":
+                    politics_flag = True
+                crawl(url[0], num_links, output, site_name, score, politics_flag)
